d236be67f572c691d6e2953bd11af79a2e8fe04c
Flow-Up12 <marcosje2005@icloud.com>
2024-07-13 18:59:57 -0500

    css improvements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,34 +7,34 @@ const Header: React.FC = () => {
 
   const [isModalOpen, setModalOpen] = useState(false);
   const [isSettingsOpen, setSettingsOpen] = useState(false);
-  
-  const {score, framesPerSecond} = useGameContext();
+
+  const { score, framesPerSecond } = useGameContext();
 
   return (
-    <header className="bg-black text-white py-4">
+    <header className="bg-black text-white py-2">
       <div className="container flex justify-between mx-auto">
         <h1 className="text-4xl font-bold">Car Game</h1>
         <div>
           <span className="text-4xl font-bold">Score: {score}</span>
         </div>
-        <div>
-        <button
-          className="bg-white text-blue-600 px-4 py-2 rounded-lg mr-5"
-          onClick={() => setModalOpen(true)}
-        >
-          Select Car
-        </button>
-        {/* settings button add icon */}
-        <button
-          className="bg-white text-blue-600 py-2 rounded-lg"
-          onClick={() => setSettingsOpen(true)}
-        >
-        Settings
-        </button>
-          {framesPerSecond}
+        <div >
+          <button
+            className="bg-white text-blue-600 px-4 rounded-lg mr-5"
+            onClick={() => setModalOpen(true)}
+          >
+            Select Car
+          </button>
+          {/* settings button add icon */}
+          <button
+            className="bg-white text-blue-600  rounded-lg"
+            onClick={() => setSettingsOpen(true)}
+          >
+            Settings
+          </button>
+          <span className='ml-5'>FPS: {framesPerSecond}</span>
         </div>
       </div>
-      
+
       {isModalOpen && <CarSelectModal closeModal={() => setModalOpen(false)} />}
       {isSettingsOpen && <SettingsModal onClose={() => setSettingsOpen(false)} isOpen={isSettingsOpen} />}
     </header>
